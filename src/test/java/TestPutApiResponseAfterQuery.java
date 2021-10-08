e4cb975aa7c641d51c172329c36db51fdeb08995
Mahesh Kafle <mahesh.kafle@smarsh.com>
2021-10-09 01:00:04 +0545

    Implemented Put Api response using RA

---
+++ src/test/java/TestPutApiResponseAfterQuery.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class TestPutApiResponseAfterQuery {
+
+    @Test
+    void put_req(){
+        RestAssured.baseURI ="https://reqres.in/api/users/2";
+        String payload = "{"+
+                "\"email\" : \"hello@gmail.com\", " +
+                "\"first_name\" : \"Mahesh\", + " +
+                "\"last_name\" : \"Kafle\", + " +
+                "}" ;
+        given()
+            .body(payload)
+        .when()
+            .put()
+        .then().log().all()
+            .assertThat().statusCode(200)
+            .body("updatedAt",is(notNullValue()));
+    }
+}
